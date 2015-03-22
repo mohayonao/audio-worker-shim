aef7005ae752436d0be64464f1f4c94634b10521
mohayonao <mohayonao@gmail.com>
2015-03-22 09:25:01 +0900

    add tests

--- test/audio-worker-shim.js
+++ test/audio-worker-shim.js
@@ -6,22 +6,42 @@ require("../");
 
 describe("AudioContext", function() {
   describe("createAudioWorker", function() {
-    it("(scriptURL: string, numberOfInputChannels: number, numberOfOutputChannels: number): AudioNode", function() {
+    it("(scriptURL: string, numberOfInputChannels: number, numberOfOutputChannels: number): AudioNode", function(done) {
       var audioContext = new global.AudioContext();
       var node = audioContext.createAudioWorker("test_worker.js");
 
       assert(node instanceof global.AudioNode);
+
+      setTimeout(done, 0);
+    });
+    it("(syntaxErrorScriptURL: string, numberOfInputChannels: number, numberOfOutputChannels: number): AudioNode", function(done) {
+      var audioContext = new global.AudioContext();
+      var node = audioContext.createAudioWorker("syntax_error.js");
+
+      assert(node instanceof global.AudioNode);
+
+      setTimeout(done, 0);
+    });
+    it("(notFoundScriptURL: string, numberOfInputChannels: number, numberOfOutputChannels: number): AudioNode", function(done) {
+      var audioContext = new global.AudioContext();
+      var node = audioContext.createAudioWorker("not_found.js");
+
+      assert(node instanceof global.AudioNode);
+
+      setTimeout(done, 0);
     });
   });
 });
 
 describe("OfflineAudioContext", function() {
   describe("createAudioWorker", function() {
-    it("(scriptURL: string, numberOfInputChannels: number, numberOfOutputChannels: number): AudioNode", function() {
+    it("(scriptURL: string, numberOfInputChannels: number, numberOfOutputChannels: number): AudioNode", function(done) {
       var audioContext = new global.OfflineAudioContext(2, 44100, 44100);
       var node = audioContext.createAudioWorker("test_worker.js");
 
       assert(node instanceof global.AudioNode);
+
+      setTimeout(done, 0);
     });
   });
 });
